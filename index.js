c852ea6d0bed9c1b6b004a1d761ab8f2dd9704ff
prajapatilalit <prajapatilalit150992@gmail.com>
2021-07-27 00:05:06 +0530

    generate player2 random number

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+//Player1
 var randomNumber1 = Math.floor(Math.random() * 6) + 1; // 1-6 random number
-var randomImage = "dice" + randomNumber1 + ".png"; // dice1 - dice6
-var randomImageSource = "images/" + randomImage; // images/dice1 - images/dice6
-var image1 = document.querySelectorAll("img")[0];
-image1.setAttribute("src", randomImageSource);
+var randomImage1 = "dice" + randomNumber1 + ".png"; // dice1 - dice6
+var randomImageSource = "images/" + randomImage1; // images/dice1 - images/dice6
+var player1 = document.querySelectorAll("img")[0];
+player1.setAttribute("src", randomImageSource);
+
+//Player2
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+randomImage2 = "images/dice" + randomNumber2 + ".png";
+var player2 = document.querySelectorAll("img")[1];
+
+player2.setAttribute("src", randomImage2);
